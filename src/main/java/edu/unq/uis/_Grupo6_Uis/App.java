b8b3490d59f5f687c872e2c48a38dc5acf936657
Dario Gutierrez <dariofg93@gmail.com>
2016-08-27 15:22:47 -0300

    initial commit

---
+++ src/main/java/edu/unq/uis/_Grupo6_Uis/App.java
@@ -0,0 +1,13 @@
+package edu.unq.uis._Grupo6_Uis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
